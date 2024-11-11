709bbf92050563ed96a5b195457e9c1679830b2b
Schyerke <93547332+Schyerke@users.noreply.github.com>
2024-11-11 09:38:21 +0100

    init

---
+++ src/test/java/com/example/gestioneazienda/GestioneaziendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestioneazienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneaziendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
